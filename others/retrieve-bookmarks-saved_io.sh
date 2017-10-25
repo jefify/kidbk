84af65f15bdf5bda9b7ac55d5aef48e16bac3793
Lin <lin@koala.cplex.corp>
2017-10-25 18:43:47 -0200

    Bookmarks do Passageiro17 - 2017-10-25 18:43

---
+++ others/retrieve-bookmarks-saved_io.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+_devkey=$1
+_key=$2
+
+if ! curl --silent "http://devapi.saved.io/bookmarks?limit=1&devkey=$_devkey&key=$_key" | grep -q bk_id; then
+  echo Nao autenticado
+  exit
+fi
+
+retrieve_list () {
+  local _l=$1
+  curl --silent "http://devapi.saved.io/bookmarks?limit=10000&devkey=$_devkey&key=$_key&list=$_l" | jq -r 'sort_by(.bk_url)|.[]| .bk_url + " " + .bk_title'
+}
+
+_lists=()
+
+retrieve_list level0 | while read url title
+do
+  if [[ $url =~ ^(.*)\.saved\.io$ ]]
+  then
+     _lists+=( "${BASH_REMATCH[1]}" )
+  else
+     echo "$title $url"
+  fi
+done
+
+for l in "${_lists[@]}"
+do
+  echo "#$l ?????"
+  retrieve_list $l | while read url title
+  do
+    if [[ $url =~ ^(.*)\.saved\.io$ ]]
+    then
+       echo "Ops ${BASH_REMATCH[1]}"
+    else
+       echo "$title $url"
+    fi
+  done
+done
